3f2c9417e17634be04de110ae21f247c74ff061f
bryant-bardales <73409340+bryant-bardales@users.noreply.github.com>
2021-11-23 21:08:32 -0500

    Added commments
    Added some comments, needs a few more.

--- index.js
+++ index.js
@@ -1,20 +1,22 @@
 const scoreDisplay = document.querySelector('.score');//Score Display
-const desks = document.querySelectorAll('.desk');//
-const prof = document.querySelectorAll('.prof');
-const prof2 = document.querySelectorAll('.prof2');
-const prof3 = document.querySelectorAll('.prof3');
-const prof4 = document.querySelectorAll('.prof4');
+const desks = document.querySelectorAll('.desk');//Desk image
+const prof = document.querySelectorAll('.prof');//Prof 1 image
+const prof2 = document.querySelectorAll('.prof2');//Prof 2 image
+const prof3 = document.querySelectorAll('.prof3');//Prof 3 image
+const prof4 = document.querySelectorAll('.prof4');//Prof 4 image
 
-let lastDesk;
-let endTime;
-let scr = 0;
+let lastDesk; //
+let endTime; //
+let scr = 0;//Inital score set to 0
 
-var mySong = document.getElementById("music");
+var mySong = document.getElementById("music");//Song that plays when the game is being played.
 
+//Function to et a random amount of time in milliseconds
 function randTime(min, max) { 
   return Math.round(Math.random() * (max - min) + min);
 }
 
+//Returns a random desk DOM element
 function randDesk(desks) {
   const i = Math.floor(Math.random() * desks.length);
   const desk = desks[i];
@@ -25,6 +27,7 @@ function randDesk(desks) {
   return desk;
 }
 
+//Function that allows the image to "pop up"
 function popOut() {
   const time = randTime(200, 1000);
   const desk = randDesk(desks);
@@ -34,14 +37,17 @@ function popOut() {
     if (!endTime) popOut();
   }, time);
 }
+
+//Function that initializes the game when start is pressed. 
 function gameStart() {
-  scoreDisplay.textContent = 0;
+  scoreDisplay.textContent = 0; //Score display is at zero
   endTime = false;
-  scr = 0;
-  popOut();
+  scr = 0;//Score is 0
+  popOut();//Images starts to "pop up"
   setTimeout(() => endTime = true, 60000)
 }
 
+//Function makes the image go down when clicked on. 
 function whack(e) {
   if(!e.isTrusted) return; 
   scr++;
@@ -50,21 +56,21 @@ function whack(e) {
 }
 
 
-
+//Allows for the user to click on the image that "pops up" and as a result the image will go down. 
 prof.forEach(prof => prof.addEventListener('click', whack));
 prof2.forEach(prof2 => prof2.addEventListener('click', whack));
 prof3.forEach(prof3 => prof3.addEventListener('click', whack));
 prof4.forEach(prof4 => prof4.addEventListener('click', whack));
 
-//automatically plays the music on website load
-//also sets volume automatically
+//Automatically plays the music on website load
+//Also sets volume automatically
 window.addEventListener("DOMContentLoaded", event => {
   const audio = document.querySelector("audio");
   audio.volume = 0.2;
   audio.play();
 });
 
-//Play and pause buttons
+//Play and pause function for the music. 
 function plays(){
   if(mySong.paused){
     mySong.play();
