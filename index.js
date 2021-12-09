68504cd577823b3d308e961cd9523ccc096e3d0d
Dzmitry Akulau <dmitry@dzmitrys-mbp.myfiosgateway.com>
2021-12-08 19:52:23 -0500

    updates

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-const scoreDisplay = document.querySelector('.score');//Score Display
+//const scoreDisplay = document.querySelector('.score');//Score Display
+const scoreDisplay = document.getElementById('score');
 const desks = document.querySelectorAll('.desk');//Desk image
 const prof = document.querySelectorAll('.prof');//Prof 1 image
 const prof2 = document.querySelectorAll('.prof2');//Prof 2 image
 const prof3 = document.querySelectorAll('.prof3');//Prof 3 image
 const prof4 = document.querySelectorAll('.prof4');//Prof 4 image
+const gameArea = document.getElementById('game');
 
 let lastDesk; //
 let endTime; //
@@ -40,9 +42,9 @@ function popOut() {
 
 //Function that initializes the game when start is pressed. 
 function gameStart() {
-  scoreDisplay.textContent = 0; //Score display is at zero
+  scoreDisplay.innerText = scr; //Score display is at zero
   endTime = false;
-  scr = 0;//Score is 0
+  //scr = 0;//Score is 0
   popOut();//Images starts to "pop up"
   setTimeout(() => endTime = true, 60000)
 }
@@ -92,4 +94,82 @@ function plays(){
     mySong.pause();
     document.getElementById("button").innerHTML = "Play Music";
   }
+}
+//updates start here
+
+//timer with implemented myFunction() conditions
+  if(!timer) {      //timer involved
+    timer = window.setInterval(function() { 
+      myFunction() 
+      
+    }, 1000); // every second
+  }
+
+
+var seconds=10;
+var timer;
+function myFunction() {
+ // CONDITIONS FOR LOSE AND WIN SCREEN
+  if(seconds <10&&scr<2) { 
+    document.getElementById("timer").innerHTML = seconds;
+  }
+  if (seconds >0 &&scr<2) { // so it doesn't go to -1
+     seconds--;
+  } 
+  else if(seconds >0&&scr>=2){  //if you kick neccessary amoumt of profs, you will get win message 
+    clearInterval(timer);
+    gameOverWin()
+  } 
+    else {    //else loose message
+     clearInterval(timer);
+     gameOver()
+   
+  }
+
+
+}
+
+document.getElementById("timer").innerHTML="10";
+
+
+//start screen
+function startGame(){
+  let start=document.getElementById("start");
+  let gameOver=document.getElementById("gameOver");
+  let gameOverWin=document.getElementById("gameOverWin");
+  //let gameArea=gameArea = document.getElementById('game');
+  start.style.display="none";
+  gameArea.style.display="flex";
+  gameOver.style.display="none";
+  gameOverWin.style.display="none";
+  document.getElementById("timer").innerHTML="10";
+  seconds=10;
+  timer = window.setInterval(function() { 
+    myFunction();
+  }, 1000);
+}
+
+
+//game over  loose screen
+function gameOver(){
+  let start=document.getElementById("start");
+  let gameOver=document.getElementById("gameOver");
+  
+  start.style.display="none";
+  gameArea.style.display="none";
+  gameOver.style.display="block";
+  document.getElementById('score').innerHTML=0;
+  scr=0;
+}
+
+//game over win screen
+function gameOverWin(){
+  let start=document.getElementById("start");
+  let gameOverWin=document.getElementById("gameOverWin");
+ 
+  start.style.display="none";
+  gameArea.style.display="none";
+  gameOverWin.style.display="block";
+  document.getElementById('score').innerHTML=0;
+  scr=0;
 }
